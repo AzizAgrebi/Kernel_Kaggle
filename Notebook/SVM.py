2252bf5e9c0d5a6e99debc6b687ca34a49adde9e
Romain Guillaumé <romaing.bis@gmail.com>
2025-03-12 06:17:36 +0100

    svm impl + very fast mismatch (et donc spectrum) kernel

---
+++ Notebook/SVM.py
@@ -0,0 +1,35 @@
+import numpy as np
+from cvxopt import matrix, solvers
+
+solvers.options['show_progress'] = False
+
+class SVM():
+    """
+    SVM implementation
+    
+    Usage:
+        svm = SVM(C=1)
+        svm.fit(kernel(X_train, X_train), y_train)
+        svm.predict_classes(kernel(X_test, X_train))
+    """
+
+    def __init__(self, C=1.0):
+        self.C = C
+
+    def fit(self, K, y):
+        K = K.astype(np.float64)
+        y = y.astype(np.float64)
+        n = K.shape[0]
+
+        P = matrix(K)
+        q = matrix(-y)
+        G = matrix( np.vstack([np.diag(y),-np.diag(y)]) )
+        h = matrix( np.hstack([np.full(n, self.C), np.zeros(n)]) )
+
+        result = solvers.qp(P=P,q=q,G=G,h=h)
+        self.alphas = np.squeeze(np.array(result['x']))
+
+    def predict(self, K):
+        y = np.dot(K, self.alphas)
+        return 2*(y > 0) - 1
+
---
+++ Notebook/mismatch_kernel.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from scipy.sparse import coo_matrix
+from numba import njit
+
+def preprocess(X):
+    tf = {c:i for i,c in enumerate('ATCG')}
+    return [np.array([tf[c] for c in x], dtype=np.int32) for x in X]
+
+@njit
+def kmer_id(kmer):
+    id = np.int32(0)
+    for c in kmer:
+        id = (id << 2) ^ c     # id = 4 * id + c 
+    return id
+
+@njit
+def extract_kmers(x, k, m=0):
+    # kmer_id(u) is the number in [0, 4**k - 1] associated to u.
+    # kmer_ids = [kmer_id(u) for u (k,m)-mer of x]
+
+    kmer_ids = np.empty((len(x) - k + 1) * (1 + m*3*k), np.int32)
+    count = 0
+    for i in range(len(x) - k + 1):
+        id = kmer_id(x[i:i+k])
+        kmer_ids[count] = id
+        count += 1
+
+        if m == 1:
+            for pos in range(k):
+                for c in range(4):
+                    neigh_id = (id & (~(3 << (pos << 1)))) | (c << (pos << 1))
+                    if neigh_id != id:
+                        kmer_ids[count] = neigh_id
+                        count += 1
+    return kmer_ids
+
+def embed(X, k, m):
+    kmers = [extract_kmers(x, k, m) for x in X]
+    n, d = len(kmers), len(kmers[0])
+    data = np.ones(n * d, dtype=np.float32)
+    rows = np.repeat(np.arange(n),d)
+    cols = np.hstack(kmers)
+    return coo_matrix((data, (rows, cols)), shape=(n, 4**k))
+
+def mismatch_kernel(X, k, m, Y=None, normalize=True):
+    """
+    Compute the mismatch_kernel(k,m) matrix of (X,Y) for m=0 or m=1.
+    Should parse the datasets almost instantaneously for m=0 (spectrum kernel) 
+    and in less than 10 seconds for m=1 (whatever k).
+    X : list[np.array[dtype=np.int32]] can be obtained as X = preprocess(iterable[str]).
+    """
+    if Y is None:
+        X_emb = embed(X, k, m)
+        K = (X_emb @ X_emb.T).toarray()
+        if normalize:
+            temp = np.sqrt(np.diag(K))
+            K = (K / temp) / temp[:,None]
+        return K
+    else:
+        X_emb, Y_emb = embed(X, k, m), embed(Y, k, m)
+        K = (X_emb @ Y_emb.T).toarray()
+        if normalize:
+            K /= np.sqrt(np.array(X_emb.power(2).sum(axis=1)))
+            K /= np.sqrt(np.array(Y_emb.power(2).sum(axis=1))).flatten()
+        return K
